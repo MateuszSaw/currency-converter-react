228f3544ed3cadbc8fcf1e38439799ad60968166
Mateusz Sawicki <mateuszsawicki1509@gmail.com>
2020-11-26 11:46:49 +0100

    added nomralize.css, font, icon, and basic html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+    <header className="header">
+      <h2>Przelicznik walut</h2>
+    </header>
+    <form className="form">
+      <p>
+        <label>
+          <span className="form__labelText">Kwota w zł:</span>
+          <input
+            className="form__field"
+            type="number"
+            placeholder="Kwota"
+            step="0.01"
+            required
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="form__labelText">Waluta:</span>
+          <select className="form__field">
+            <option value="EUR">Euro</option>
+            <option value="GBP">Funt brytyjski</option>
+            <option value="USD">Dolar amerykański</option>
+          </select>
+        </label>
+      </p>
+      <button className="button">Przelicz</button>
+      <p><strong className="result"></strong></p>
+    </form>
+  </div>
+  )
 }
 
 export default App;
